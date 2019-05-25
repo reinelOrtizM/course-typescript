2856b888c6712cc4c7dadbe31270c1e8a739e1ad
rort <rort@rort.com>
2019-05-24 19:10:16 -0500

    Cargue Base Proyecto

---
+++ app/index.ts
@@ -0,0 +1,7 @@
+class App {
+    startApp() {
+        console.log(">> Start my First App");
+    }
+}
+
+new App().startApp();
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: "./app/index.ts",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+    watch: true,
+    devtool: "soruce-map",
+    resolve: {
+        // Add '.ts' and '.tsx' as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js", "json"]
+    },
+    module: {
+        rules: [
+            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+    externals:{},
+    mode: "production"
+};
